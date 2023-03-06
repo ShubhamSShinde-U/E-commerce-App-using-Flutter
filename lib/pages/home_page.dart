4bdb62655c0379d0e5a318e1b4479391834df666
Shubham Shinde <shubhamshindec1@gmail.com>
2023-03-06 19:06:03 +0530

    getting error

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,20 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
-import 'package:flutter/services.dart';
 import 'package:flutter_application_1/core/store.dart';
 import 'package:flutter_application_1/models/cart.dart';
 import 'package:flutter_application_1/utils/routes.dart';
 import 'package:flutter_application_1/widgets/home_widgets/catalog_list.dart';
 import 'package:velocity_x/velocity_x.dart';
-
 import 'package:flutter_application_1/models/catalog.dart';
-import 'package:flutter_application_1/widgets/themes.dart';
+import 'package:http/http.dart' as http;
 //dart convert: json encoder decoder
 import 'dart:convert';
 
 import '../widgets/home_widgets/caatalog_header.dart';
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -23,6 +23,7 @@ class _HomePageState extends State<HomePage> {
   final int day = 20;
 
   final String name = "Shubham";
+  final url ="https://api.jsonbin.io/v3/b/6405e540c0e7653a05834f86";
 
   @override
   void initState() {//agar data haya pehle aa jaye to mai build method ko directly de sakta hu: so initState used
@@ -31,16 +32,15 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async{
-    //to load data from JSON file we can use root bundle which is placed in service library
-    var catalogJson = await rootBundle.loadString("assests/files/catalog.json");//this will return future so we have to wait so add await at start 
-
-    var decodedData = jsonDecode(catalogJson);
+    
+    await Future.delayed(const Duration(seconds: 2));
+    final response = await http.get(Uri.parse(url));
+    final catalogJson = response.body;
+    final decodedData = jsonDecode(catalogJson);
     var productData = decodedData["products"];
 
-    CatalogModel.items = List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();//getting list of items
-    // print(productData);//here  I will get mapped data
-
-    setState(() {});//set state to fetch all data
+    CatalogModel.items = List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
   }
   @override
   Widget build(BuildContext context){//context is element which decides location of each widget
